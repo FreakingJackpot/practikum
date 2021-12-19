1c94808aaa850851c79755f3bdb59661560daac6
jackpot <sss30112001@yandex.ru>
2021-12-20 03:33:41 +0400

    add thousand separator

--- practikum/settings.py
+++ practikum/settings.py
@@ -149,3 +149,5 @@ SHOW_TOOLBAR_CALLBACK = show_toolbar
 MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
 django_heroku.settings(locals())
 
+USE_L10N = True
+USE_THOUSAND_SEPARATOR = True
