0fd86a21301b9c992ba47d80ec938884e79207af
jackpot <sss30112001@yandex.ru>
2022-01-07 01:40:01 +0400

    add import test

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.core.files import File
+from catalog.models import Product, Category, AttributeValue
+from catalog.services.import_products_from_excel import ExcelProductImporter
 
-# Create your tests here.
+
+class ImportProductsTestCase(TestCase):
+
+    def test_import(self):
+        from os import getcwd
+        print(getcwd())
+        with open('./catalog/fixtures/test.xlsx', 'rb') as f:
+            importer = ExcelProductImporter(f)
+            importer.run()
+
+        categories_count = Category.objects.count()
+        products_count = Product.objects.count()
+        value = AttributeValue.objects.filter(value='Ширина: 90 см X Высота: 190 см X Глубина: 40 см').first()
+
+        self.assertEqual(2, categories_count)
+        self.assertEqual(11, products_count)
+        self.assertTrue(value)
+        self.assertEqual(1, value.product_id)
