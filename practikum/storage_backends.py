7ecf922f1ed789ad2f4c61f008d8212071afc63c
jackpot <sss30112001@yandex.ru>
2022-01-09 22:09:54 +0400

    fix

--- practikum/storage_backends.py
+++ practikum/storage_backends.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage, S
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class PublicMediaStorage(S3Boto3Storage):
